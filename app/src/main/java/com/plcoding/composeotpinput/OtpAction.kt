8967d190ba99483c7df7fe92ed7922d712deb5a5
Philipp Lackner <mail@pl-coding.com>
2024-11-28 17:00:14 +0100

    Initial commit

---
+++ app/src/main/java/com/plcoding/composeotpinput/OtpAction.kt
@@ -0,0 +1,7 @@
+package com.plcoding.composeotpinput
+
+sealed interface OtpAction {
+    data class OnEnterNumber(val number: Int?, val index: Int): OtpAction
+    data class OnChangeFieldFocused(val index: Int): OtpAction
+    data object OnKeyboardBack: OtpAction
+}
---
+++ app/src/main/java/com/plcoding/composeotpinput/OtpState.kt
@@ -0,0 +1,7 @@
+package com.plcoding.composeotpinput
+
+data class OtpState(
+    val code: List<Int?> = (1..4).map { null },
+    val focusedIndex: Int? = null,
+    val isValid: Boolean? = null
+)
